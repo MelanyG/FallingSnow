70d3c53d497d088310c5896a95af10c348e6d6e5
Melany Gulianovych <mhul@gmail.com>
2020-03-24 11:16:51 +0200

    Added image change

--- FallingSnow/ViewController.swift
+++ FallingSnow/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            let alertWarning = UIAlertController(title: "Помилка",
+                                                 message: "Error",
+                                                 preferredStyle: UIAlertController.Style.alert)
+            let cancelAction = UIAlertAction(title: "OK", style: .cancel) {
+                UIAlertAction in
+            }
+            alertWarning.addAction(cancelAction)
+            present(alertWarning, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        imageView.image = image
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        openCamera()
+    }
 }
 
